462cc5b88de0659de4b35201e500d7c26fb3e5a1
jogoiarexx-blip <jogoiarexx@gmail.com>
2026-09-21 17:55:53 -0300

    chore: versão 2.2.3

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 // pelo botão Configurações do próprio app ou preencha driveApiKey abaixo. Pastas com
 // manifest.json + páginas WebP numeradas são lidas página a página, sem baixar um CBR/CBZ inteiro.
 window.MHQR_CONFIG = {
-  appVersion: '2.2.2',
+  appVersion: '2.2.3',
   folderIds: ['1e-gclwa21fdNBuGyoCaucMUEekTws8_g', '1Ly_9LzZht815cVzUsLPfR4BwvSYOilzK', '1VmG0IF3bZwRXHxQ-k1g7euycikJAKZPw', '1xVUuHmicfUlKj30fZ7h0vMf9ipdzvFjy', '1sZ0-6AUXUdcLXPN6uDMbSAjk8qs_Wq7F'],
   folderUrls: ['https://drive.google.com/drive/folders/1e-gclwa21fdNBuGyoCaucMUEekTws8_g', 'https://drive.google.com/drive/folders/1Ly_9LzZht815cVzUsLPfR4BwvSYOilzK', 'https://drive.google.com/drive/folders/1VmG0IF3bZwRXHxQ-k1g7euycikJAKZPw', 'https://drive.google.com/drive/folders/1xVUuHmicfUlKj30fZ7h0vMf9ipdzvFjy', 'https://drive.google.com/drive/folders/1sZ0-6AUXUdcLXPN6uDMbSAjk8qs_Wq7F'],
   folderId: '1e-gclwa21fdNBuGyoCaucMUEekTws8_g',
