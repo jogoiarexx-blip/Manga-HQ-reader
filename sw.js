6bada82abde925a1c18e1ad884a4aee0e9deb8db
jogoiarexx-blip <jogoiarexx@gmail.com>
2026-09-21 17:36:18 -0300

    chore: renovar cache PWA para v2.2.0

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'manga-hq-reader-ghpages-';
-const CACHE = `${CACHE_PREFIX}v2.1.0`;
+const CACHE = `${CACHE_PREFIX}v2.2.0`;
 const BASE = new URL('./', self.location.href);
 const CORE = [
   './', './index.html', './css/app.css', './js/app.js', './config.js',
